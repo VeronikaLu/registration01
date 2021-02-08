d7b16cf52f179e50dae7f954143a2bb459529fab
VeronikaLu <78722305+VeronikaLu@users.noreply.github.com>
2021-02-08 09:59:18 +0200

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,68 @@
+<?php 
+	require 'db.php';
+
+	$data = $_POST;
+
+
+
+	//если кликнули на button
+	if ( isset($data['do_signup']) )
+	{
+    // проверка формы на пустоту полей
+		$errors = array();
+		if ( trim($data['login']) == '' )
+		{
+			$errors[] = 'Введите логин';
+		}
+
+
+		if ( $data['password'] == '' )
+		{
+			$errors[] = 'Введите пароль';
+		}
+
+		if ( $data['password_2'] != $data['password'] )
+		{
+			$errors[] = 'Повторный пароль введен не верно!';
+		}
+
+		//проверка на существование одинакового логина
+		if ( R::count('users', "login = ?", array($data['login'])) > 0)
+		{
+			$errors[] = 'Пользователь с таким логином уже существует!';
+		}
+
+		if ( empty($errors) )
+		{
+			//ошибок нет, теперь регистрируем
+			$user = R::dispense('users');
+			$user->login = $data['login'];
+			$user->password = password_hash($data['password'], PASSWORD_DEFAULT); //пароль нельзя хранить в открытом виде, мы его шифруем при помощи функции password_hash для php > 5.6
+			R::store($user);
+			echo '<div style="color:green;">Вы успешно зарегистрированы!</div><hr>';
+		}else
+		{
+			echo '<div id="errors" style="color:red;">' .array_shift($errors). '</div><hr>';
+		}
+
+	}
+
+?>
+!HTML
+
+<form action="/signup.php" method="POST">
+	<strong>Ваш логин</strong>
+	<input type="text" name="login" value="<?php echo @$data['login']; ?>"><br/>
+
+
+	<strong>Ваш пароль</strong>
+	<input type="password" name="password" value="<?php echo @$data['password']; ?>"><br/>
+
+	<strong>Повторите пароль</strong>
+	<input type="password" name="password_2" value="<?php echo @$data['password_2']; ?>"><br/>
+
+	<strong><?php captcha_show(); ?></strong>
+	<input type="text" name="captcha" ><br/>
+
+	<button type="submit" name="do_signup">Регистрация</button>
+</form>
